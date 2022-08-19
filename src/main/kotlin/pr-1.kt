b6f2341a3a946ed4e54cd60e58c309e971d1ccf5
yeshachauhan3 <chauhanyesha1@gmail.com>
2022-08-19 15:43:40 +0530

    pr-1

--- src/main/kotlin/pr-1.kt
+++ src/main/kotlin/pr-1.kt
@@ -1,3 +1,36 @@
+//1.Store & Display values in different variable of different type (Integer, Double, Float, Long, Short, Byte, Char, Boolean, String)
+
 fun main(){
-    println('Hello world')
+    var a:Int
+    var b:Float
+    var c:Char
+    var d:String
+    var e:Boolean
+    var f:Double
+    var g:Long
+    var h:Short
+    var i:Byte
+
+    a=11
+    b=1.2f
+    c='Y'
+    d="Yesha"
+    e= true
+    f=932.36
+    g=9321
+    h=-1
+    i=0
+
+    println("Integer Value = ${a}")
+    println("Float Value = ${b}")
+    println("Character Value = ${c}")
+    println("String Vlaue = ${d}")
+    println("Boolean Value = ${e}")
+    println("Double Value = ${f}")
+    println("Long Value = ${g}")
+    println("Short Value = ${h}")
+    println("Byte Value = ${i}")
+
+
+
 }
